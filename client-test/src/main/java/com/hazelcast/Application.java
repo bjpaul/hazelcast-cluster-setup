8b34d18b4aed38efe8ac7dad6ff745289f1e2efe
bjpaul <kgec.bijoypaul@gmail.com>
2017-04-23 20:46:47 +0530

    .gitignore modified for idea project

---
+++ client-test/src/main/java/com/hazelcast/Application.java
@@ -0,0 +1,11 @@
+package com.hazelcast;
+
+/**
+ * Created by bijoypaul on 23/04/17.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+//        ConnectionFactory.newClient();
+    }
+}
